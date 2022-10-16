c03450da6f43672033c39310dbccae7bd1fd41f8
Samandar0021305 <TSamandardev@gmail.com>
2022-10-16 11:29:39 +0500

    init

---
+++ src/components/ThemeProvider.jsx
@@ -0,0 +1,26 @@
+import React , {useContext , useState , useEffect, createContext} from "react";
+const ThemeContext = createContext()
+
+function ThemeProvider(props){
+    const [theme,setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || false);
+    
+    useEffect(()=>{
+     localStorage.setItem("theme",JSON.stringify(theme))
+    }, [theme])
+
+    const setThemeMode = mode => setTheme(mode)
+
+    return(
+        <ThemeContext.Provider value={{theme , setThemeMode}}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+
+}
+
+const useThemeHook =()=>{
+    const {theme} = useContext(ThemeContext)
+    return [theme]
+}
+
+export default ThemeProvider;
